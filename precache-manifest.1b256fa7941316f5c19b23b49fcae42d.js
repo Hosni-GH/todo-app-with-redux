6c0093374db836f5272bed37809165f6b04f3d41
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-16 10:02:19 +0100

    Updates

---
+++ precache-manifest.1b256fa7941316f5c19b23b49fcae42d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6b29ae97b0da1f39589a5cbc55d9f5",
+    "url": "/todo-app-with-redux/index.html"
+  },
+  {
+    "revision": "c18535b01137668bc53a",
+    "url": "/todo-app-with-redux/static/css/main.60554fb6.chunk.css"
+  },
+  {
+    "revision": "74aa27b676cb5be351c5",
+    "url": "/todo-app-with-redux/static/js/2.7d05a767.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-app-with-redux/static/js/2.7d05a767.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c18535b01137668bc53a",
+    "url": "/todo-app-with-redux/static/js/main.d66774dd.chunk.js"
+  },
+  {
+    "revision": "26f01b2c68fb67e47344",
+    "url": "/todo-app-with-redux/static/js/runtime-main.0445e93e.js"
+  }
+]);
